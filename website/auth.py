c94aa0df8cc90fe2a44d070667b9914b68d8641e
Shaul1Kr <shaulikr07@gmail.com>
2021-12-11 21:44:43 +0200

    Fixed project
    Fixed project

---
+++ website/auth.py
@@ -0,0 +1,66 @@
+import re
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+from datetime import timedelta, datetime, time
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/', methods=['GET', 'POST'])
+def login():
+    if 'signup' in request.form:
+        return redirect(url_for('auth.signup'))
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.homePage'))
+
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("index.html", user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confPassword = request.form.get('passwordconfirm')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif password != confPassword:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, password=generate_password_hash(
+                password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()        
+            flash('Account created!', category='success')
+            login_user(new_user, remember=True)
+            return redirect(url_for('views.homePage'))
+        
+    return render_template("signup.html", user=current_user)    
---
+++ website/views.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for,session
+from flask_login import login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import random
+from . import db
+import json
+from .models import User
+
+views = Blueprint('views', __name__)
+
+# Home page
+
+
+@views.route("/homepage")
+@login_required
+def homePage():
+    return render_template("homepage.html")
+
+# Photos page
+@views.route("/photos")
+@login_required
+def photos():
+    photos = ["static/photos/1.jpg", "static/photos/2.jpg", "static/photos/3.jpg",
+              "static/photos/4.jpg", "static/photos/5.jpg", "static/photos/6.jpg", "static/photos/7.jpg"]
+    return render_template("photos.html", photos= photos)
+
+# User Settings
+@views.route("/setting")
+@login_required
+def userSettings():
+    return render_template("usersettings.html")
