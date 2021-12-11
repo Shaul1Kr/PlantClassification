96c45779965e2658e6e1561141d828476141e73d
Shaul1Kr <shaulikr07@gmail.com>
2021-12-11 16:21:37 +0200

    db and some more pages
    db and some more pages

--- app.py
+++ app.py
@@ -1,31 +1,78 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 from datetime import timedelta
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.secret_key = "hello"
-app.permanent_session_lifetime = timedelta(days=1)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.permanent_session_lifetime = timedelta(minutes=2)
 
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+#Login page
 @app.route("/")
 def home():
-    return redirect(url_for("login"))
+    return render_template("index.html")
+#Sing up page
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/homepage")
+def homePage():
+    return render_template("homepage.html")
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         user = request.form["nm"]
         session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
         return redirect(url_for("user"))
     else:
         return render_template("login.html")
 
-@app.route("/user")
+
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        if request.method == "POST":
+            email = request.form["email"]
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            session["email"] = email
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
     else:
         return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
