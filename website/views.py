4284c198bf849ca5d00db2ed8eea8c6091c0e7de
Dor Rozenhak <dor851997@gmail.com>
2022-05-07 16:16:42 +0300

    add util.py

--- website/views.py
+++ website/views.py
@@ -11,28 +11,45 @@ from tensorflow import keras
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+import numpy as np
+from util import base64_to_pil
+
 
 views = Blueprint('views', __name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH_1 = './models/UrbanPlantClassifier.h5'
+MODEL_PATH_1 = 'website/model/bestmodel_23class.hdf5'
+# Load my own trained model
+model = keras.models.load_model(MODEL_PATH_1)
+print('Model loaded.. Check http://127.0.0.1:5000/')
+print('Model loaded. Start serving...')
+
+def model_predict1(img, model):
+    # Preprocessing the image
+    img = img.resize((256, 256))
+    x = image.img_to_array(img) / 255
+    x = np.expand_dims(x, axis=0)
+    preds = model.predict(x)
+    return preds
 
 # Home page
-
 
 @views.route("/homepage")
 @login_required
 def homePage():
+    # Make prediction
+    #preds1 = model_predict1(base64_to_pil('website/static/photos/1.jpg'), model)
+    #print(preds1)
     return render_template("homepage.html", user=current_user)
 
 # Photos page
-
 
 @views.route("/photos")
 @login_required
 def photos():
     photos = ["static/photos/1.jpg", "static/photos/2.jpg", "static/photos/3.jpg",
               "static/photos/4.jpg", "static/photos/5.jpg", "static/photos/6.jpg", "static/photos/7.jpg"]
+    
     return render_template("photos.html", photos=photos, user=current_user)
 
 # User Settings
@@ -58,3 +75,5 @@ def about():
 @login_required
 def contact():
     return render_template("contact.html", user=current_user)
+
+
