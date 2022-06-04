32fb686f9d40540166165888279135234905d389
Dor Rozenhak <dor851997@gmail.com>
2022-06-04 19:40:16 +0300

    att

--- website/views.py
+++ website/views.py
@@ -14,14 +14,15 @@ from tensorflow import keras
 import numpy as np
 from util import base64_to_pil
 
-from tensorflow.keras.preprocessing import image
+from keras.preprocessing import image
 
 
 views = Blueprint('views', __name__)
 
 # Model saved with Keras model.save()
 #MODEL_PATH_1 = './model/bestmodel_23class.hdf5'
-MODEL_PATH_1 = '../model/bestmodel_23class.hdf5'
+#MODEL_PATH_1 = '../model/bestmodel_23class.hdf5'
+MODEL_PATH_1 = "website/static/bestmodel_23class.hdf5"
 # Load my own trained model
 model_1 = keras.models.load_model(MODEL_PATH_1)
 print('Model loaded.. Check http://127.0.0.1:5000/')
